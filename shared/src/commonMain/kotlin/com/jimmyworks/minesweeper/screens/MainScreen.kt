3c32227e754e546e8a83b235775bca1ed365beca
Jimmy Kang <af19git5@gmail.com>
2024-08-30 16:36:56 +0800

    優化防呆顯示畫面

--- shared/src/commonMain/kotlin/com/jimmyworks/minesweeper/screens/MainScreen.kt
+++ shared/src/commonMain/kotlin/com/jimmyworks/minesweeper/screens/MainScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import com.jimmyworks.minesweeper.component.MainComponent
+import com.jimmyworks.minesweeper.styles.Colors
 import com.jimmyworks.minesweeper.styles.PaddingStyle
 import com.jimmyworks.minesweeper.styles.TextStyle
 import com.jimmyworks.minesweeper.views.Toast
@@ -35,15 +36,12 @@ import minesweeper.shared.generated.resources.Res
 import minesweeper.shared.generated.resources.app_name
 import minesweeper.shared.generated.resources.count
 import minesweeper.shared.generated.resources.game_size
+import minesweeper.shared.generated.resources.game_size_notice
 import minesweeper.shared.generated.resources.go
 import minesweeper.shared.generated.resources.ic_mines
-import minesweeper.shared.generated.resources.mines_over_notice
 import minesweeper.shared.generated.resources.number_of_mines
-import minesweeper.shared.generated.resources.x_less_notice
-import minesweeper.shared.generated.resources.y_less_notice
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
-import kotlin.math.min
 
 /**
  * 主畫面
@@ -59,25 +57,15 @@ fun MainScreen(component: MainComponent) {
     val toastMessage = component.toastVO.subscribeAsState()
 
     // 提示訊息
-    val xLessNotice = stringResource(Res.string.x_less_notice)
-    val yLessNotice = stringResource(Res.string.y_less_notice)
-    val minesLessNotice = stringResource(Res.string.mines_over_notice)
+    val gameSizeNotice = stringResource(Res.string.game_size_notice)
 
     // 鍵盤控制器
     val keyboardController = LocalSoftwareKeyboardController.current
 
     /** 啟動遊戲 */
     fun gameStart() {
-        if (component.x.value < 1) {
-            component.toastVO.value = ToastVO(xLessNotice)
-            return
-        }
-        if (component.y.value < 1) {
-            component.toastVO.value = ToastVO(yLessNotice)
-            return
-        }
-        if (component.minesCount.value >= component.x.value * component.y.value) {
-            component.toastVO.value = ToastVO(minesLessNotice)
+        if (component.x.value !in 1..30 || component.y.value !in 1..30) {
+            component.toastVO.value = ToastVO(gameSizeNotice)
             return
         }
         component.onGameStart()
@@ -108,32 +96,48 @@ fun MainScreen(component: MainComponent) {
             Row(
                 Modifier.padding(top = 16.dp), verticalAlignment = Alignment.CenterVertically
             ) {
-                Text("X: ", style = TextStyle.h3Style)
+                Text(
+                    "X: ", style = TextStyle.h3Style,
+                    color = if (component.x.value in 1..30) Colors.primary else Colors.error
+                )
                 OutlinedTextField(
                     value = x.toString(),
-                    onValueChange = { component.x.value = min(it.toIntOrNull() ?: 1, 30) },
+                    onValueChange = { component.x.value = it.toIntOrNull() ?: 0 },
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Number, imeAction = ImeAction.Next
                     ),
+                    isError = component.x.value !in 1..30,
                     textStyle = TextStyle.textStyle,
                     modifier = Modifier.padding(start = 5.dp).width(80.dp)
                 )
                 Text(
-                    "Y: ", style = TextStyle.h3Style, modifier = Modifier.padding(start = 18.dp)
+                    "Y: ",
+                    style = TextStyle.h3Style,
+                    modifier = Modifier.padding(start = PaddingStyle.p3Style),
+                    color = if (component.y.value in 1..30) Colors.primary else Colors.error
                 )
                 OutlinedTextField(
                     value = y.toString(),
-                    onValueChange = { component.y.value = min(it.toIntOrNull() ?: 1, 30) },
+                    onValueChange = { component.y.value = it.toIntOrNull() ?: 0 },
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Number, imeAction = ImeAction.Next
                     ),
+                    isError = component.y.value !in 1..30,
                     textStyle = TextStyle.textStyle,
                     modifier = Modifier.padding(start = 5.dp).width(80.dp)
                 )
             }
+            if (component.x.value !in 1..30 || component.y.value !in 1..30) {
+                Text(
+                    gameSizeNotice,
+                    style = TextStyle.textStyle,
+                    color = Colors.error,
+                    modifier = Modifier.padding(top = PaddingStyle.p5Style),
+                )
+            }
             Text(
                 stringResource(Res.string.number_of_mines),
-                style = TextStyle.h3Style,
+                style = TextStyle.h2Style,
                 modifier = Modifier.padding(top = 30.dp)
             )
             Row(
